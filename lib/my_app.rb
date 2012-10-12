6ac279b4de880e38cdf9255c0bb35ea90b286acb
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-10-12 00:16:03 -0500

    adicion de view

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -2,10 +2,14 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   get '/' do
-  	"Comentario"
-#    erb:index.erb
+   	erb:principal
   end
-
+  get '/registro' do
+   	erb:registro
+  end
+  get '/falta' do
+   	erb:falta
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
