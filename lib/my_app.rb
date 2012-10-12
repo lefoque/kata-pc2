9068ce840b064c69c28f72aafa6fbbcac226a6f1
lefoque <loliva71@gmail.com>
2012-10-12 04:43:09 -0500

    Update lib/my_app.rb
    Completando la practica

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,15 +1,28 @@
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
+
   get '/' do
-   	erb:principal
+    erb :principal
   end
-  get '/registro' do
-   	erb:registro
-  end
-  get '/falta' do
-   	erb:falta
-  end
+    post "/registro" do
+
+      @pNombre   = params[:pNombre] 
+      @pEmail    = params[:pEmail] 
+      @pComentario = params[:pComentario] 
+
+      if @pNombre == "" or @pEmail == "" or @pComentario == ""
+
+          erb :falta
+
+      else
+
+          erb :registro
+
+      end
+
+    end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
